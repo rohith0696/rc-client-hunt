625e7552e1adcd56c1af196e188604df57367b67
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-01-24 20:08:38 -0600

    adding all files

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', (main) => {
+    console.log("page is fully loaded");
+  });
+
+locationElement.addEventListener('click', locationHandler);
+locationElement.addEventListener('touch', locationHandler);
