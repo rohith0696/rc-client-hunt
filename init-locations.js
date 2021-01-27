ba64aded82c9780742e8942f055b39b9a97143df
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-01-27 09:56:14 -0600

    added location

---
+++ init-locations.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      Name: 'Colden Hall',
+      latitude:40.35112431021874, 
+      longitude:-94.8824199980626,
+      Notes: 'circle',
+      Attachments: '',
+      'Quest-Location': 'Colden Hall'
+    },
+    {
+      Name: 'Hughes Fieldhouse',
+      latitude:40.3540856, 
+      longitude:-94.8881649,
+      Notes: 'circle',
+      Attachments: '',
+      'Quest-Location': 'Hughes Fieldhouse'
+    }];
